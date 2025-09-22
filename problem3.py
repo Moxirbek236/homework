4a2b990890960f0f0fb4b33d4aab7bd416c7d116
moxirbek29 <nasimjonnasimjon46@gmail.com>
2025-09-22 15:30:01 +0500

    add

--- problem3.py
+++ problem3.py
@@ -2,13 +2,13 @@ def calculate_painting_time(pattern: list) -> int:
     if not pattern:
         return 0
     
-    time = 1  # Start with 1 minute for the first section
+    time = 1 
     time_count = 1
     
     for i in range(1, len(pattern)):
-        time += 2  # Increment time if the color changes
+        time += 2 
         if pattern[i] != pattern[i - 1]:
-            time_count += 1  # Reset time count for a new color
+            time_count += 1 
     
     return time + time_count
 
