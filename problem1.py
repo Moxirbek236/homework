8c12d8e28bd765893124985de3055a285ca91166
moxirbek29 <nasimjonnasimjon46@gmail.com>
2025-09-22 15:26:53 +0500

    add nev verse1

--- problem1.py
+++ problem1.py
@@ -1,10 +1,6 @@
 def merge_unique_ids(active_users: list, new_users: list) -> list:
-    unique_ids = []
-    
-    for user_id in active_users + new_users:
-        if user_id not in unique_ids:
-            unique_ids.append(user_id)
-    
+    unique_ids = set(user_id for user_id in active_users + new_users if user_id in active_users + new_users)
+    unique_ids = list(sorted(unique_ids))
     return unique_ids
 
 active_users = ['U1001', 'U1002', 'U1003', 'U1004', 'U1001']
